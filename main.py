2292f29626aab55e71aad48f253c8b68f8717fba
mayank-ind <83276551+mayank-ind@users.noreply.github.com>
2023-01-28 08:41:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,18 @@ bot = Client(
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH")
 )
+with bot:
+
+    BOT = bot.get_me().username.lower()
+
+auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
+
+sudo_groups = [ int(chat) for chat in os.environ.get("GROUPS").split(",")  if chat != '']
+
+
+
+
+sudo_users = auth_users
 
 logger = logging.getLogger()
 
